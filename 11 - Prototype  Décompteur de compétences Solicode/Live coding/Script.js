baccc16b5c9c983051a5b012b8525115b7e305ff
mohamed aghbalou <mohamedaghbalou.solicode@gmail.com>
2025-09-23 15:27:59 +0100

    1

--- 11 - Prototype  Décompteur de compétences Solicode/Live coding/Script.js
+++ 11 - Prototype  Décompteur de compétences Solicode/Live coding/Script.js
@@ -1,11 +1,11 @@
-numbers = ["c1","c3","c2","c1","c8","c1" ];
-let choisi = "c1"; 
+let numbers = ["c1","c3","c2","c1","c8","c1" ]; //Array - مصفوفة
+let choisi = "c1"; //le code choisi a calculer
 let counter = 0;
-for (i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] === choisi) {
-        counter++;
+        counter++  // ; counter= couter +1
 
     }
 }
-console.log("Le code " + choisi + " apparaît " + counter + " fois.");
+console.log("La valeur " + choisi + " apparaît " + counter + " fois.");
     
