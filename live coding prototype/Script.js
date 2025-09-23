8b16ccf558bec3dedad9b6076afaa0c9f8f2b3e3
mohamed aghbalou <mohamedaghbalou.solicode@gmail.com>
2025-09-23 15:27:17 +0100

    one more edit

--- live coding prototype/Script.js
+++ live coding prototype/Script.js
@@ -1,7 +1,7 @@
-numbers = ["c1","c3","c2","c1","c8","c1" ]; //Array - مصفوفة
+let numbers = ["c1","c3","c2","c1","c8","c1" ]; //Array - مصفوفة
 let choisi = "c1"; //le code choisi a calculer
 let counter = 0;
-for (i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] === choisi) {
         counter++  // ; counter= couter +1
 
