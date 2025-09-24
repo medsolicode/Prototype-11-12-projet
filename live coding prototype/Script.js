7e7bbb1d51447ef82dbc1c76dc99b45fbcfe05d8
med <mohamedaghbalou.solicode@gmail.com>
2025-09-24 13:33:24 +0100

    init

--- live coding prototype/Script.js
+++ live coding prototype/Script.js
@@ -2,11 +2,10 @@ function alpha() {
 let numbers = ["c1","c3","c2","c1","c8","c1" ]; //Array
 let choisi = "c1"; //le code choisi a calculer
 let counter = 0; 
-for (let i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++)  {
     if (numbers[i] === choisi) {
         counter++  // ; counter= couter +1
     }
 }
-console.log("La valeur " + choisi + " apparaît " + counter + " fois.");
-
+console.log("La valeur " + choisi + " = " + counter + " fois.");
 }alpha();
